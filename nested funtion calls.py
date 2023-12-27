3a8e245b50fd3faf29f9a836975cf67493197e89
gokosavage <gokselmarufoglu@gmail.com>
2023-12-27 21:26:05 +0300

    8. Gün

---
+++ nested funtion calls.py
@@ -0,0 +1,9 @@
+#num = input("Enter a whole positive number: ")
+#num = float(num)
+#num = abs(num)
+#num = round(num)
+
+#print(num)
+
+print(round(abs(float(input("Enter a whole positive number: "))))) #içten dışa doğru(önce float sonra abs sonra round en son print)
+
---
+++ variable scope.py
@@ -0,0 +1,8 @@
+name = "Göks" #global scope (avaliable inside & outside functions)
+
+def display_name():
+    #name = "M" #local scope (avaliable only inside this function)
+    print(name)
+
+display_name()
+print(name)
